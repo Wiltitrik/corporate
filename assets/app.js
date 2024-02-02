2150555a25248657e3cea807543286d190bd4fe8
Wilfried <wiltitrik@gmail.com>
2024-02-02 15:31:31 +0100

    TD Corporate

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,9 @@ import './bootstrap.js';
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+//  */
+import './styles/app.scss';
+
+import '../var/sass/app.output.css'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
